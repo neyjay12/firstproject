1561c354e2624dd35b3398acbd2446a82407de1e
Victor <einejeameh70@gmail.com>
2025-04-16 12:59:56 -0400

    Made a rock,paper and scissors game using the random module

--- Ramdomization _list/Randomization_and_list.py
+++ Ramdomization _list/Randomization_and_list.py
@@ -4,5 +4,4 @@ friends = ["Alice", "Bob", "Charlie", "David", "Emmanuel"]
 
 pay_the_bill = random.sample(friends, 1)
 
-
 print(pay_the_bill)
---
+++ Ramdomization _list/Rock_paper_scissors.py
@@ -0,0 +1,30 @@
+import random
+
+computer_choice = random.choice(['rock', 'paper', 'scissors'])
+
+# print(computer_choice)
+
+print("Welcome to the Rock, paper, scissors game!")
+
+user1 = input("Type 0 for Rock, 1 for Paper, 2 for Scissors: ")
+
+if user1 == '0' and computer_choice == 'rock':
+    print("Its a Draw!")
+elif user1 == '0' and computer_choice == 'paper':
+    print("You loose!")
+elif user1 == '0' and computer_choice == 'scissors':
+    print("You win!")
+elif user1 == '1' and computer_choice == 'rock':
+    print("You win!")
+elif user1 == '1' and computer_choice == 'paper':
+    print("Its a Draw!")
+elif user1 == '1' and computer_choice == 'scissors':
+    print("You loose!")
+elif user1 == '2' and computer_choice == 'rock':
+    print("You loose!")
+elif user1 == '2' and computer_choice == 'paper':
+    print("You win!")
+elif user1 == '2' and computer_choice == 'scissors':
+    print("Its a Draw!")
+else:
+    print("Invalid choice!")
