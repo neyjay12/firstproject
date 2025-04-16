b90fe41fecba1be3fcd1ec3dc5acb37933f045d7
Victor <einejeameh70@gmail.com>
2025-04-16 13:03:54 -0400

    Made a rock,paper and scissors game using the random module

--- Ramdomization _list/Rock_paper_scissors.py
+++ Ramdomization _list/Rock_paper_scissors.py
@@ -9,22 +9,31 @@ print("Welcome to the Rock, paper, scissors game!")
 user1 = input("Type 0 for Rock, 1 for Paper, 2 for Scissors: ")
 
 if user1 == '0' and computer_choice == 'rock':
+    print(f"Computer chose {computer_choice}")
     print("Its a Draw!")
 elif user1 == '0' and computer_choice == 'paper':
+    print(f"Computer chose {computer_choice}")
     print("You loose!")
 elif user1 == '0' and computer_choice == 'scissors':
+    print(f"Computer chose {computer_choice}")
     print("You win!")
 elif user1 == '1' and computer_choice == 'rock':
+    print(f"Computer chose {computer_choice}")
     print("You win!")
 elif user1 == '1' and computer_choice == 'paper':
+    print(f"Computer chose {computer_choice}")
     print("Its a Draw!")
 elif user1 == '1' and computer_choice == 'scissors':
+    print(f"Computer chose {computer_choice}")
     print("You loose!")
 elif user1 == '2' and computer_choice == 'rock':
+    print(f"Computer chose {computer_choice}")
     print("You loose!")
 elif user1 == '2' and computer_choice == 'paper':
+    print(f"Computer chose {computer_choice}")
     print("You win!")
 elif user1 == '2' and computer_choice == 'scissors':
+    print(f"Computer chose {computer_choice}")
     print("Its a Draw!")
 else:
     print("Invalid choice!")
